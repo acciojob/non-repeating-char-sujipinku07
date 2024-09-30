908faed197bdcd4dbe5bdad431caf0b87df8b6be
sujipinku07 <sujipinku07@gmail.com>
2024-09-30 18:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Create a frequency map to store the count of each character
- const charCount = {};
- // Count the frequency of each character in the string
- for (let char of str) {
- charCount[char] = (charCount[char] || 0) + 1;
- }
- // Iterate through the string again to find the first non-repeated character
- 
- }
- for (let char of str) {
- if (charCount[char] === 1) {
- return char;
- }
- }
- // If no non-repeated character is found, return null
- return null;
+	const freq={};
+	for(let char of str){
+		if(freq[char]){
+			freq[char]++;
+		}
+		else{
+			freq[char]=1;
+		}
+	}
+	for(let char of str){
+		if(freq[char]==1){
+			return char;
+		}
+	}
+	return null;
 }
+
+ const input = prompt("Enter a string");
+const result = firstNonRepeatedChar(input);
